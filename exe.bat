ed2bf0024d2afceb5a8002c943bdd38a48c7f32d
Danielodf2 <danisnake27@gmail.com>
2021-06-10 13:10:58 -0300

    add jar

--- exe.bat
+++ exe.bat
@@ -1,1 +1,1 @@
-java --module-path "C:\Program Files\Java\javafx-sdk-16\lib" --add-modules javafx.controls,javafx.fxml -jar F:\Softwares to my\DanP\Danp.jar
+java --module-path "C:\Program Files\Java\javafx-sdk-16\lib" --add-modules javafx.controls,javafx.fxml,javafx.media -jar F:\Softwares to my\DanP\DanP.jar
