454353ad838248e6a1fe1cf82ce0a20c432efcf6
Danielodf2 <danisnake27@gmail.com>
2021-06-10 04:10:13 -0300

    add software

---
+++ src/application/Main.java
@@ -0,0 +1,34 @@
+package application;
+
+import javafx.application.Application;
+
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+
+
+public class Main extends Application {
+	private static Stage stage;
+	private static Scene sc;
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		stage = primaryStage;
+		AnchorPane root = FXMLLoader.load(getClass().getResource("Tela2.fxml"));
+	    sc = new Scene(root);
+		stage.getIcons().add(new Image("file:imagens/astronauta.jpg"));
+		stage.setScene(sc);
+		stage.setTitle("DAMP");
+		stage.setResizable(false);
+
+		stage.show();
+ 
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/application/MainController.java
@@ -0,0 +1,269 @@
+package application;
+
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
+import javafx.application.Platform;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.FileChooser;
+import javafx.util.Duration;
+
+public class MainController {
+	@FXML
+	private AnchorPane anchorPane;
+	@FXML
+	private Slider barra;
+	@FXML
+	private Slider barraVolume;
+	@FXML
+	private Label lblNmusica, lblTitulo,lblTempo, lblDuracao;
+	@FXML
+	private Button btnPlay, btnPause, btnAvancar, btnVoltar;
+	private Duration resumePlayer;
+	private File directory;
+	private File[] files;
+	private ArrayList<File> musica;
+	private Timer timer;
+	private MediaPlayer danp;
+	private Media mp;
+	private boolean ok = false;
+	private static String CaminhoMusica;
+	int musicaNumero;
+	
+
+	public void EscolhaCaminho() {
+		try {
+			FileChooser fc = new FileChooser();
+			fc.setTitle("Escolha uma musica");
+			fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("MP3", "*.mp3*"));
+			File file = fc.showOpenDialog(null);
+
+		if (file != null) {
+			lblTitulo.setText(file.getName().replace("MP3", "*.mp3*"));
+			CaminhoMusica = file.getAbsolutePath();
+			play();
+			BarraAvancar();
+			DuracaoTotal();
+		} 
+		}catch(NullPointerException e) {
+		
+		}catch(Exception e) {
+			MSA("Arquivo Invalido");
+		}
+	}
+
+	public void play() {
+		try {
+		if (ok == false) {
+			mp = new Media(Paths.get(CaminhoMusica).toUri().toString());
+			danp = new MediaPlayer(mp);
+			danp.play();
+			ok = true;
+			retorna();
+			BarraAvancar();
+			reloop();
+			Temporizador();
+			volume();
+	
+		} else {
+			danp.pause();
+			cancelaBarra();
+			ok = false;
+		}
+
+	}catch(NullPointerException e){
+		MSA("Escolha alguma música!");
+	}
+}
+	public void cancelaBarra() {
+		ok = false;
+		timer.cancel();
+	}
+
+	public void pause() {
+		cancelaBarra();
+		danp.pause();
+	}
+
+	public void voltar() {
+		if (musicaNumero > 0) {
+			musicaNumero--;
+			danp.stop();
+			if (ok) {
+				cancelaBarra();
+			}
+			mp = new Media(musica.get(musicaNumero).toURI().toString());
+			danp = new MediaPlayer(mp);
+			lblNmusica.setText(musica.get(musicaNumero).getName());
+			play();
+		} else {
+			musicaNumero = musica.size() - 1;
+			danp.stop();
+			if (ok) {
+				cancelaBarra();
+			}
+			mp = new Media(musica.get(musicaNumero).toURI().toString());
+			danp = new MediaPlayer(mp);
+			lblNmusica.setText(musica.get(musicaNumero).getName());
+			play();
+		}
+	}
+
+	public void avancar() {
+		if (musicaNumero < musica.size() - 1) {
+			musicaNumero++;
+			danp.stop();
+			if (ok) {
+				cancelaBarra();
+			}
+			mp = new Media(musica.get(musicaNumero).toURI().toString());
+			danp = new MediaPlayer(mp);
+			lblNmusica.setText(musica.get(musicaNumero).getName());
+			play();
+		} else {
+			musicaNumero = 0;
+			danp.stop();
+			mp = new Media(musica.get(musicaNumero).toURI().toString());
+			danp = new MediaPlayer(mp);
+			lblNmusica.setText(musica.get(musicaNumero).getName());
+			play();
+		}
+	}
+
+	public void volume() {
+		barraVolume.valueProperty().addListener(new javafx.beans.value.ChangeListener<Number>() {
+
+	    	 @Override
+			public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
+				danp.setVolume(barraVolume.getValue() * 0.01);
+			}
+		});
+		barra.setStyle("-fx-accent: #00FF00;");
+	
+	}
+
+	public void BarraAvancar() {
+		timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+				double currentSeconds = danp.getCurrentTime().toSeconds();
+				double end = mp.getDuration().toSeconds();
+				barra.setMax(end);
+				barra.setValue(currentSeconds);
+
+			}
+		}, 0, 1000);
+	}
+	
+	public void MSA (String k){
+		Alert alert = new Alert (AlertType.WARNING);
+		alert.setTitle("Atenção");
+		alert.setContentText(k);
+		alert.setHeaderText(null);
+		alert.showAndWait();
+		
+		}
+	
+	public void SliderTime() {
+		new Thread(() -> 
+		danp.seek(Duration.seconds(barra.getValue()))).start();
+			}
+	
+	public void Temporizador() {
+		timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+				Platform.runLater(() -> {
+					int Time = (int) danp.getCurrentTime().toMillis();
+					int minutes = (Time / 60000) % 60000;
+					int seconds = Time % 60000 / 1000;
+					String CurrentTime = String.format("%02d:%02d", minutes, seconds);
+					lblTempo.setText(CurrentTime);
+				});
+
+			}
+		}, 0, 1000);
+	}
+	
+	public void DuracaoTotal() {
+		danp.setOnReady(new Runnable() {
+			
+			@Override
+			public void run() {
+				int tempoDuracao = (int) mp.getDuration().toMillis();
+				int minutos = (tempoDuracao / 60000) % 60000;
+				int segundos = tempoDuracao % 60000 / 1000;
+				String CurrentTime = String.format("%02d:%02d", minutos, segundos);
+				lblDuracao.setText(CurrentTime);
+				
+				int Tempo = (int) danp.getCurrentTime().toMillis();
+				int minutosAnda = (Tempo / 60000) % 60000;
+				int segundosAnda = Tempo % 60000 / 1000;
+				String tempoAnda = String.format("%02d:%02d", minutosAnda, segundosAnda);
+				lblTempo.setText(tempoAnda);
+				
+			}
+		});
+	}
+	
+	public void retorna() {
+		timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask() {
+			
+			@Override
+			public void run() {
+				Platform.runLater(() -> {
+				if(resumePlayer != null && ok == true) {
+					danp.setStartTime(resumePlayer);
+				}else {
+					resumePlayer = danp.getCurrentTime();
+				}
+				});
+			}
+		},0,1000);
+	}
+	
+	public void reloop() {
+		timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask() {
+			
+			@Override
+			public void run() {
+				Platform.runLater(() -> { 
+				try {
+					  if(danp.getCurrentTime().toSeconds() == mp.getDuration().toSeconds()) {
+							resumePlayer = null;
+							danp.setStartTime(resumePlayer);
+							play();
+						}
+					  }catch(Exception e ) {}
+				  });
+				 
+				  } }, 0, 1000);	
+	}
+	
+	public void Starway () {
+		if(ok == true) {
+			play();
+			EscolhaCaminho();;
+		}else {
+			EscolhaCaminho();
+		}
+	}
+
+}
